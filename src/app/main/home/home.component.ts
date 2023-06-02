7f6c33e601e6f823ae3ff9a9f3e64482f9acabe2
horusen <kane.babacar@protonmail.com>
2023-06-02 13:38:54 +0000

    Requick

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { BaseComponent } from 'src/app/shared/base-component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
+export class HomeComponent extends BaseComponent<any> {
+  constructor(public productService: ProductService) {
+    super();
+    this.data = productService.data.slice(0, 6);
+  }
 }
