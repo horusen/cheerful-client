62653e1c935cfcba744cfb5113b5f46c644e2c32
horusen <kane.babacar@protonmail.com>
2023-09-16 07:29:33 +0000

    Authcomponent added
    This serve the purpose of encapsulating common styles
    in among the auth components. It also has the router-outlet
    of the auth module allowing the router to navigate within
    all authmodule's components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,29 +6,43 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input-gg';
 import { JoinComponent } from './join/join.component';
+import { AuthComponent } from './auth.component';
+import { OtpComponent } from './otp/otp.component';
 
 const routes: Routes = [
   {
-    path: 'join',
-    component: JoinComponent,
-  },
-  {
-    path: 'signup',
-    component: SignupComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
     path: '',
-    redirectTo: 'signup',
-    pathMatch: 'full',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'join',
+        component: JoinComponent,
+      },
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'signup',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
 
 @NgModule({
-  declarations: [SignupComponent, LoginComponent, JoinComponent],
+  declarations: [
+    SignupComponent,
+    LoginComponent,
+    JoinComponent,
+    AuthComponent,
+    OtpComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
