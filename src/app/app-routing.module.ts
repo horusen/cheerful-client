53d262b503e41728b43e2178aea44c317daa02ea
horusen <kane.babacar@protonmail.com>
2023-09-16 07:29:33 +0000

    authentication route redirection added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
+    path: 'auth',
+    redirectTo: 'authentication',
+  },
+  {
     path: '',
     loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
   },
